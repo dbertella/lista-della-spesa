e14f086bee43e3777890846b435e8a44fc781f0a
unknown <dbertella@PC-PCHPDEV.extra.local>
2014-05-23 17:46:59 +0200

    lista della spesa funzionante senza db

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,4 +16,20 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+app.controller('TodoCtrl', function ($scope) {
+  $scope.data = {
+    showDelete: false,
+    showForm: false
+  }
+
+  $scope.todos = []
+
+  $scope.addTodo = function() {
+    $scope.todos.unshift( { name: $scope.todoText } );
+    //alert('pippo:' + $scope.todoText + ';');
+    $scope.todoText = '';
+    $scope.data.showForm = false;
+  };
 })
